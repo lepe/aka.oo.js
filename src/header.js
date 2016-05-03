5056dd80d4edd593db9dbcab19c97ca9268e75c8
lepe <dev@alepe.com>
2016-05-03 11:37:23 +0900

    Changed to MIT license

--- src/header.js
+++ src/header.js
@@ -1,5 +1,5 @@
 /**
  * widgets.ui.js 1.0.1
  * Author: A. Lepe (www.alepe.com)
- * Released under GPL 3.0 http://www.gnu.org/licenses/gpl-3.0.en.html
+ * Released under MIT license https://opensource.org/licenses/MIT
  */
